dac7551defae8306dda3aa867673349b4c8c09ea
junngo <myeongjun.ko@gmail.com>
2019-04-02 22:49:08 +0900

    added import env

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
-import dotenv from "dotenv";
-import path from "path";
+import "./env";
+
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import passport from "passport";
 import schema from "./schema";
 import "./passport";
 
-dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const PORT = process.env.PORT || 4000;
 
