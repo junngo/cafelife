ebbb8fbff88dd9e81ebc767449356c28793ab2ed
junngo <myeongjun.ko@gmail.com>
2019-04-11 23:46:52 +0900

    added editUser and seeUser resolver

---
+++ src/api/User/seeUser/seeUser.js
@@ -0,0 +1,12 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query: {
+        seeUser: (_, args, { request, isAuthenticated }) => {
+            isAuthenticated(request);
+            const { id } = args;
+            
+            return prisma.user({ id });
+        }
+    }
+};
