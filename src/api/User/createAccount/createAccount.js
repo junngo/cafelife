d9ac31bb7b01960e4a884dbd34d2062bffe49d7c
junngo <myeongjun.ko@gmail.com>
2019-10-01 12:04:29 +0900

    modifed createAcount

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,15 +3,29 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation: {
         createAccount: async (_, args) => {
-            const { username, email, lastName="", bio=""} = args;
+            const { username, email, lastName="", firstName="", bio = "" } = args;
+            const exists = await prisma.$exists.user({
+                OR: [
+                  {
+                    username
+                  },
+                  { email }
+                ]
+            });
+
+            if (exists) {
+                throw Error("This username / email is already taken");
+            }
+
             const user = await prisma.createUser({
                 username,
                 email,
+                firstName,
                 lastName,
                 bio
             });
 
-            return user;
+            return true;
         }
     }
 }
